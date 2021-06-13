e118a22e647ab32dc3baefea4a36c1be5d4e3242
cristiane.vanda@etec.sp.gov.br <crisvanda@gmail.com>
2021-06-12 23:25:06 -0300

    Criamos a página de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,31 +4,23 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  
+  {
+    path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
-  {
-    path: 'mortal-kombat',
-    loadChildren: () => import('./filmes/mortal-kombat/mortal-kombat.module').then( m => m.MortalKombatPageModule)
-  },
-  {
-    path: 'liga-justica',
-    loadChildren: () => import('./filmes/liga-justica/liga-justica.module').then( m => m.LigaJusticaPageModule)
-  },
-  {
-    path: 'rogaipor-nos',
-    loadChildren: () => import('./filmes/rogaipor-nos/rogaipor-nos.module').then( m => m.RogaiporNosPageModule)
-  },
+  
   {
     path: 'dados-filme',
     loadChildren: () => import('./dados-filme/dados-filme.module').then( m => m.DadosFilmePageModule)
   },
+  
   {
-    path: 'the-virtuoso',
-    loadChildren: () => import('./filmes/the-virtuoso/the-virtuoso.module').then( m => m.TheVirtuosoPageModule)
-  },
-  {
-    path: 'raya',
-    loadChildren: () => import('./filmes/raya/raya.module').then( m => m.RayaPageModule)
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   }
 ];
 @NgModule({
